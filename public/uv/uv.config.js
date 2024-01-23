1ec2c74bc246d1534bbc5d4821c8604b7a6fe17a
Aviixxyy <124846438+Aviixxyy@users.noreply.github.com>
2024-01-23 08:56:40 -0700

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,12 +1,10 @@
-// This file overwrites the stock UV config.js
-
 self.__uv$config = {
-  prefix: "/static/tiw/",
-  bare: "https://bare.benrogo.net",
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/static/uv/uv.handler.js",
-  bundle: "/static/uv/uv.bundle.js",
-  config: "/static/uv/uv.config.js",
-  sw: "/static/uv/uv.sw.js",
+    prefix: '/static/tiw/',
+    bare:'https://bare.benrogo.net',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/static/uv/uv.handler.js',
+    bundle: '/static/uv/uv.bundle.js',
+    config: '/static/uv/uv.config.js',
+    sw: '/static/uv/uv.sw.js',
 };
